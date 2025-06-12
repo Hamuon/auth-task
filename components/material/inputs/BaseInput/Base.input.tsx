cca5335f41ef6a3d28faf4a01d58fe14a4894198
hamun <hamunjavadzadeghan@gmail.com>
2025-06-12 14:43:44 +0330

    fix: seperated base input props types

--- components/material/inputs/BaseInput/Base.input.tsx
+++ components/material/inputs/BaseInput/Base.input.tsx
@@ -1,10 +1,19 @@
-import styles from './Base.input.module.scss';
+import { FC } from "react";
+import styles from "./Base.input.module.scss";
 
-export default function BaseInput({ label, value, onChange }: { label: string; value: string; onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+export interface BaseInputProps {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const BaseInput: FC<BaseInputProps> = ({ label, value, onChange }) => {
   return (
     <div className={styles.inputGroup}>
       <label>{label}</label>
       <input type="text" value={value} onChange={onChange} />
     </div>
   );
-}
+};
+
+export default BaseInput;
